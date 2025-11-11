2fbbab98a03cb56aa43770c15219a215bc2b03af
George Pamfilis <gpamfilis@gmail.com>
2025-11-11 18:40:52 +0200

    fix: add 'use client' to MaterialFeedbackButton (uses useMediaQuery)

--- src/material/MaterialFeedbackButton.tsx
+++ src/material/MaterialFeedbackButton.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState, useEffect, useMemo } from "react";
 import { Button, Box, useMediaQuery, useTheme } from "@mui/material";
 import FeedbackIcon from "@mui/icons-material/Feedback";
